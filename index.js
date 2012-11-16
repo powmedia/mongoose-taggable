b7885328add1eae6e28ad9a134640977800a5529
Charles Davison <charlie@powmedia.co.uk>
2012-11-16 12:53:14 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,97 @@
+var _ = require('underscore');
+
+
+/**
+ * Mongoose plugin
+ * 
+ * Adds the ability to add and remove tags to a document, and find models filtered by tags.
+ * Uses safe atomic updates to avoid race conditions.
+ * Meant to be used in a similar way to GitHub Issue's 'labels' feature.
+ *
+ * NOTE: Indexes are not applied to the tags path, because you should probably
+ * use a compound index with other paths in your schema
+ *
+ * @param {Schema} schema
+ * @param {Object} [pluginOptions]
+ * @param {String} [pluginOptions.path]           Path/key where tags will be stored. Default: 'tags'
+*/
+module.exports = function(schema, pluginOptions) {
+  pluginOptions = _.extend({
+    path: 'tags'
+  }, pluginOptions);
+
+  //Add paths to schema
+  var paths = {};
+  paths[pluginOptions.path] = [String];
+
+  schema.add(paths);
+
+
+  /**
+   * Add a tag to a document atomically
+   * 
+   * NOTE: This method modifies the document on the database
+   *
+   * @param {String} tag    The tag to add
+   * @param {Function} cb   Callback(err, addedTag)  addedTag will be false if tag already existed; true if added
+   */
+  schema.methods.addTag = function(tag, cb) {
+    //Find the doc  to update if it doesn't have the tag
+    var conditions = {};
+    conditions._id = this._id;
+    conditions[pluginOptions.path] = { $ne: tag };
+
+    //Add the tag to the model
+    var update = {};
+    update.$push = {};
+    update.$push[pluginOptions.path] = tag;
+
+    this.constructor.update(conditions, update, function(err, numDocsChanged) {
+      if (err) return cb(err);
+
+      cb(null, numDocsChanged ? true : false);
+    });
+  };
+
+
+  /**
+   * Remove a tag to a document atomically
+   * 
+   * NOTE: This method modifies the document on the database
+   *
+   * @param {String} tag    The tag to remove
+   * @param {Function} cb   Callback(err, removedTag)  removedTag will be false if tag didn't exist; true if removed
+   */
+  schema.methods.removeTag = function(tag, cb) {
+    //Find the doc  to update if it doesn't have the tag
+    var conditions = {};
+    conditions._id = this._id;
+    conditions[pluginOptions.path] = tag;
+
+    //Remove the tag from the model
+    var update = {};
+    update.$pull = {};
+    update.$pull[pluginOptions.path] = tag;
+
+    this.constructor.update(conditions, update, function(err, numDocsChanged) {
+      if (err) return cb(err);
+
+      cb(null, numDocsChanged ? true : false);
+    });
+  };
+
+
+  /**
+   * Alters a query to filter by tags.
+   *
+   * @param {Query} query       Mongoose query object
+   * @param {String[]} tags     Array of tags to filter by
+   */
+  schema.statics.filterByTags = function(query, tags) {
+    if (!tags || !tags.length) return query;
+    
+    query.where(pluginOptions.path).all(tags);
+
+    return query;
+  }
+};
---
+++ test.js
@@ -0,0 +1,194 @@
+//Nodeunit tests
+
+process.env.NODE_ENV = 'test';
+
+var _ = require('underscore'),
+    mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    fixtures = require('pow-mongodb-fixtures').connect('mongoose-taggable'),
+    id = require('pow-mongodb-fixtures').createObjectId,
+    sinon = require('sinon');
+
+mongoose.connect('mongodb://localhost/mongoose-taggable', function(err) {
+  //if (err) throw err;
+});
+
+
+//The module under test
+var taggable = require('./index.js');
+
+
+//Create a model to test
+var ItemSchema = new Schema({ title: String });
+ItemSchema.plugin(taggable, { path: 'labels' });
+
+var Item = mongoose.model('Item', ItemSchema);
+
+
+exports['adds paths to schema'] = {
+  'with defaults': function(test) {
+    var schema = new Schema({ title: String });
+
+    schema.plugin(taggable);
+
+    test.ok(schema.paths.tags);
+
+    test.done();
+  },
+
+  'with "path" option': function(test) {
+    var schema = new Schema({ title: String });
+
+    schema.plugin(taggable, { path: 'myTags' });
+
+    test.ok(schema.paths.myTags);
+
+    test.done();
+  }
+}
+
+
+exports['addTag'] = {
+  setUp: function(done) {
+    this.item = new Item({ labels: ['foo'] });
+
+    this.item.save(done);
+  },
+
+  'adds a tag atomically': function(test) {
+    var item = this.item;
+
+    item.addTag('bar', function(err, addedTag) {
+      if (err) return test.done(err);
+
+      test.same(addedTag, true);
+
+      Item.findById(item._id, function(err, item) {
+        if (err) return test.done(err);
+
+        test.same(item.labels.slice(), ['foo', 'bar']);
+
+        test.done();
+      });
+    });
+  },
+
+  'does not add again if the tag already exists': function(test) {
+    var item = this.item;
+
+    item.addTag('foo', function(err, addedTag) {
+      if (err) return test.done(err);
+
+      test.same(addedTag, false);
+
+      Item.findById(item._id, function(err, item) {
+        if (err) return test.done(err);
+
+        test.same(item.labels.slice(), ['foo']);
+
+        test.done();
+      });
+    });
+  }
+}
+
+
+exports['removeTag'] = {
+  setUp: function(done) {
+    this.item = new Item({ labels: ['foo', 'bar', 'baz'] });
+
+    this.item.save(done);
+  },
+
+  'removes a tag atomically': function(test) {
+    var item = this.item;
+
+    item.removeTag('bar', function(err, removedTag) {
+      if (err) return test.done(err);
+
+      test.same(removedTag, true);
+
+      Item.findById(item._id, function(err, item) {
+        if (err) return test.done(err);
+
+        test.same(item.labels.slice(), ['foo', 'baz']);
+
+        test.done();
+      });
+    });
+  },
+
+  'tagWasRemoved is false if tag didnt exist': function(test) {
+    var item = this.item;
+
+    item.removeTag('bla', function(err, removedTag) {
+      if (err) return test.done(err);
+
+      test.same(removedTag, false);
+
+      test.done();
+    });
+  }
+}
+
+
+exports['filterByTags'] = {
+  setUp: function(done) {
+    var items = [
+      { title: 'A', labels: ['a', 'b'] },
+      { title: 'B', labels: [] },
+      { title: 'C', labels: ['c', 'b'] },
+      { title: 'D', labels: ['a', 'c'] }
+    ];
+
+    fixtures.clearAndLoad({ items: items }, done);
+  },
+
+  'with one tag': function(test) {
+    var query = Item.find({});
+
+    Item.filterByTags(query, ['a']);
+
+    query.exec(function(err, docs) {
+      if (err) return test.done(err);
+
+      var titles = _.pluck(docs, 'title');
+
+      test.same(titles, ['A', 'D']);
+
+      test.done();
+    });
+  },
+
+  'with multiple tags': function(test) {
+    var query = Item.find({});
+
+    Item.filterByTags(query, ['b', 'c']);
+
+    query.exec(function(err, docs) {
+      if (err) return test.done(err);
+
+      var titles = _.pluck(docs, 'title');      
+
+      test.same(titles, ['C']);
+
+      test.done();
+    });
+  },
+
+  'with no tags, does not filter': function(test) {
+    var query = Item.find({});
+
+    Item.filterByTags(query, []);
+
+    query.exec(function(err, docs) {
+      if (err) return test.done(err);
+
+      var titles = _.pluck(docs, 'title');      
+
+      test.same(titles, ['A', 'B', 'C', 'D']);
+
+      test.done();
+    });
+  }
+}
